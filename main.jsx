06de83605e64d9331dfbccdaa7b21ba207567fe6
TG <thomasgillianlabrador@gmail.com>
2026-05-21 12:28:34 +0200

    Refactor main.jsx to simplify imports and rendering

--- main.jsx
+++ main.jsx
@@ -1,14 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'primereact/resources/themes/lara-light-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import './index.css'
 
-import "primereact/resources/themes/lara-light-indigo/theme.css";  // Theme
-import "primereact/resources/primereact.min.css";                  // Core CSS
-import "primeicons/primeicons.css";                                        // Icons
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
